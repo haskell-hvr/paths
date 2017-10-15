525ed77dca528768b414fce2f121406d2fcf9e2e
Herbert Valerio Riedel <hvr@gnu.org>
2017-10-15 12:30:38 +0200

    Add 'Text' I/O operations with fixed UTF-8 encoding

--- src/System/Path/IO.hs
+++ src/System/Path/IO.hs
@@ -22,12 +22,20 @@ module System.Path.IO
   , appendLazyByteString
   , appendStrictByteString
     -- ** Wrappers around "Data.Text.IO" and "Data.Text.Lazy.IO"
+    -- *** Locale-dependent encoding
   , readLazyText
   , readStrictText
   , writeLazyText
   , writeStrictText
   , appendLazyText
   , appendStrictText
+    -- *** [UTF-8](https://en.wikipedia.org/wiki/UTF-8) encoding
+  , readLazyTextUtf8
+  , readStrictTextUtf8
+  , writeLazyTextUtf8
+  , writeStrictTextUtf8
+  , appendLazyTextUtf8
+  , appendStrictTextUtf8
     -- ** Wrappers around "System.Directory"
   , copyFile
   , createDirectory
@@ -60,26 +68,36 @@ module System.Path.IO
 import           System.Path
 
 #if !MIN_VERSION_base(4,8,0)
-import           Control.Applicative   ((<$>))
+import           Control.Applicative      ((<$>))
 #endif
+
+import           Control.Exception        (evaluate)
 import           Control.Monad
-import           Data.Time             (UTCTime)
-import           System.IO             (BufferMode (..), Handle, IOMode (..),
-                                        SeekMode (..))
-import           System.IO.Unsafe      (unsafeInterleaveIO)
+import           Data.Time                (UTCTime)
+import           System.IO                (BufferMode (..), Handle, IOMode (..),
+                                           SeekMode (..))
+import qualified System.IO                as IO
+import           System.IO.Unsafe         (unsafeInterleaveIO)
+
 #if !MIN_VERSION_directory(1,2,0)
-import           Data.Time.Clock       (picosecondsToDiffTime)
-import           Data.Time.Clock.POSIX (posixSecondsToUTCTime)
-import           System.Time           (ClockTime (TOD))
+import           Data.Time.Clock          (picosecondsToDiffTime)
+import           Data.Time.Clock.POSIX    (posixSecondsToUTCTime)
+import           System.Time              (ClockTime (TOD))
 #endif
-import qualified Data.ByteString       as BS
-import qualified Data.ByteString.Lazy  as BS.L
-import qualified Data.Text             as T
-import qualified Data.Text.IO          as T
-import qualified Data.Text.Lazy        as T.L
-import qualified Data.Text.Lazy.IO     as T.L
-import qualified System.Directory      as Dir
-import qualified System.IO             as IO
+
+import qualified Data.ByteString          as BS
+import qualified Data.ByteString.Lazy     as BS.L
+import qualified Data.Text                as T
+import qualified Data.Text.Encoding       as T.E
+import qualified Data.Text.IO             as T
+import qualified Data.Text.Lazy           as T.L
+import qualified Data.Text.Lazy.Encoding  as T.L.E
+import qualified Data.Text.Lazy.IO        as T.L
+import qualified System.Directory         as Dir
+
+#if defined(__HADDOCK_VERSION__)
+import           Data.Text.Encoding.Error (UnicodeException)
+#endif
 
 {-------------------------------------------------------------------------------
   Wrappers around System.IO
@@ -144,6 +162,9 @@ appendStrictByteString path bs = do
   Wrappers around Data.Text.*
 -------------------------------------------------------------------------------}
 
+--------------------------------------------------------------------------------
+-- locale-dependent versions
+
 -- | Wrapper around lazy 'T.L.readFile'
 readLazyText :: FsRoot root => Path root -> IO T.L.Text
 readLazyText path = do
@@ -177,6 +198,47 @@ appendLazyText path bs = do
 -- | Wrapper around strict 'T.appendFile'
 appendStrictText :: FsRoot root => Path root -> T.Text -> IO ()
 appendStrictText path bs = do
+    filePath <- toAbsoluteFilePath path
+    T.appendFile filePath bs
+
+--------------------------------------------------------------------------------
+-- UTF-8 versions
+
+-- | Read lazy 'Text' from a file (using UTF-8 encoding).
+--
+-- __NOTE__: Since the file is read lazily UTF-8 decoding errors are detected lazily as well. Such errors will result in an 'Data.Text.Encoding.Error.UnicodeException' being thrown within the lazy 'Text' stream.
+readLazyTextUtf8 :: FsRoot root => Path root -> IO T.L.Text
+readLazyTextUtf8 path = T.L.E.decodeUtf8 <$> readLazyByteString path
+
+-- | Read strict 'Text' from a file (using UTF-8 encoding).
+--
+-- __NOTE__: In case of UTF-8 decoding errors an 'Data.Text.Encoding.Error.UnicodeException' will be thrown.
+readStrictTextUtf8 :: FsRoot root => Path root -> IO T.Text
+readStrictTextUtf8 path = do
+    bs <- readStrictByteString path
+    evaluate (T.E.decodeUtf8 bs)
+
+-- | Write lazy 'Text' to a file (using UTF-8 encoding). The file is truncated to zero length before writing begins.
+writeLazyTextUtf8 :: FsRoot root => Path root -> T.L.Text -> IO ()
+writeLazyTextUtf8 path bs = do
+    filePath <- toAbsoluteFilePath path
+    T.L.writeFile filePath bs
+
+-- | Write strict 'Text' to a file (using UTF-8 encoding). The file is truncated to zero length before writing begins.
+writeStrictTextUtf8 :: FsRoot root => Path root -> T.Text -> IO ()
+writeStrictTextUtf8 path bs = do
+    filePath <- toAbsoluteFilePath path
+    T.writeFile filePath bs
+
+-- | Append lazy 'Text' to end of a file (using UTF-8 encoding).
+appendLazyTextUtf8 :: FsRoot root => Path root -> T.L.Text -> IO ()
+appendLazyTextUtf8 path bs = do
+    filePath <- toAbsoluteFilePath path
+    T.L.appendFile filePath bs
+
+-- | Append strict 'Text' to end of a file (using UTF-8 encoding).
+appendStrictTextUtf8 :: FsRoot root => Path root -> T.Text -> IO ()
+appendStrictTextUtf8 path bs = do
     filePath <- toAbsoluteFilePath path
     T.appendFile filePath bs
 
